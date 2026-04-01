d374e2a97d964fd65979e768c6d81285f1da08d3
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-01 11:10:16 +0300

    Task 8: Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	num = 'a';
+		while (num <= 'f')
+		{
+			putchar(num);
+			num++;
+		}
+	putchar('\n');
+	return (0);
+}
+
