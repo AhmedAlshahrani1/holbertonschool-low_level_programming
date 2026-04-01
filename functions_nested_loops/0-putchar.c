ad75fc5909d72b46209f879d38808f2e8d167256
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-01 17:50:20 +0300

    Finish task 0 and add project documentation

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+	char str[] = "_putchar";
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
+
