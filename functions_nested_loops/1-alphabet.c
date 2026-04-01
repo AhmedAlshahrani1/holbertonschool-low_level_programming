db645c2d7bf44be234db64c0d257cb65c3625c39
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-02 02:05:27 +0300

    Task 1: Add print_alphabet function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * void - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
