1fb73060bf06befc4f29b9ad934e5a3869492389
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-04 20:15:52 +0300

    Task 4: Add print_most_numbers function

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 9)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
+		c++;
+	}
+	_putchar('\n');
+}
