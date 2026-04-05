52c12b6841f3ae842b244e08d77cd4c596e19ff2
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-05 16:12:17 +0300

    Task 6: Add print_line function

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int c = 0;
+
+	while (c < n)
+	{
+		_putchar('_');
+		c++;
+	}
+	_putchar('\n');
+}
