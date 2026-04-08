50df01d52b86c99ac0848547fb0f25590cfaea03
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-08 19:33:13 +0300

    Add Task 6: Half the lies they tell about me aren't true (puts2 function)

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i +=2;
+	}
+	_putchar('\n');
+}
