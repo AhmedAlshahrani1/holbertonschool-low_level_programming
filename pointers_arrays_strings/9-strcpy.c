dc0500faaffa2e04562d5e8bcd8bfbfa869b7f30
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-09 15:52:42 +0300

    Add Task 9: strcpy (copy string from src to dest including null byte)

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src, 
- * including the terminating null byte (\0), 
- * to the buffer pointed to by dest.
- * @dest: pointer to the destination array
- * @src: pointer to the source string
- * Return: the pointer to dest
+ * _strcpy - copies a string from src to dest
+ * @dest: pointer to destination buffer
+ * @src: pointer to source string
+ *
+ * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
