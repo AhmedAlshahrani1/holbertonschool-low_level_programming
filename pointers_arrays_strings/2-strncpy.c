d4ece9ef4952d103562bb8e98918106322e59997
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-10 18:18:56 +0300

    Task 2: strncpy implementation

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: the destination buffer
+ * @src: the source string
+ * @n: the maximum number of bytes to copy
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
+	return (dest);
+}
