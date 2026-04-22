e9f22a8404feaacbb8de566928051e591f2f0b42
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-22 16:54:19 +0300

    Task 3: Implement alloc_grid for 2D array allocation with memory safety

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: pointer to 2D array, or NULL if failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < height)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			j = 0;
+			while (j < i)
+			{
+				free(grid[j]);
+				j++;
+			}
+			free(grid);
+			return (NULL);
+		}
+		j = 0;
+		while (j < width)
+		{
+			grid[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+
+	return (grid);
+}
