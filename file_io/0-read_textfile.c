1a5227daa06ba89bbd17fd28a6f80038886fd150
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-25 23:08:10 +0300

    Task 0: Read a text file and print it

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t len_r, len_w;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+
+	len_r = read(fd, buf, letters);
+	if (len_r == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	len_w = write(STDOUT_FILENO, buf, len_r);
+	if (len_w == -1 || len_w != len_r)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+	return (len_w);
+}
