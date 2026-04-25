dcfa8962aba3b182cc40f45eb16c9e58b3ac5507
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-04-25 20:14:55 +0300

    Task 8: Delete node at index without comments

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (-1);
+
+	tmp = *head;
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+
+	while (tmp && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (!tmp)
+		return (-1);
+
+	tmp->prev->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
+
+	free(tmp);
+	return (1);
+}
