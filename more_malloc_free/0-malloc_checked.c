b910c2110eb019aafc249b38eb79d6800df626ef
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-05-11 17:30:46 +0300

    Add Task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to allocate.
+ *
+ * Return: A pointer to the allocated memory.
+ *         If malloc fails, terminates normal process with status 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard library includes if needed strictly for size_t etc, though limits apply */
+#include <stdlib.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
