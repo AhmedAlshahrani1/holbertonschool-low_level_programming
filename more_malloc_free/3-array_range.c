f2745ad5b81bb3e538e62c4e10067889d8d95a8e
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-05-13 13:09:23 +0300

    Add Task 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 	size = max - min + 1;
-	p = malloc (sizeof(int) * size);
+	p = malloc(sizeof(int) * size);
 	if (p == NULL)
 	{
 		return (NULL);
