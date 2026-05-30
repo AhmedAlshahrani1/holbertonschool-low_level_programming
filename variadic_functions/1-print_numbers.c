910fbd3608a17cb7f2620aa4cc8157a3f6d16482
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-05-30 22:25:46 +0300

    Fix the Betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,10 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		/* طباعة الرقم الحالي */
 		printf("%d", va_arg(args, int));
-
-		/* طباعة الفاصل إذا لم يكن الأخير وإذا لم يكن فارغاً */
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
